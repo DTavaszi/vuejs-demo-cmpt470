6a31b6272cbbfb5f3ca18d705651af2f388fb02a
dtavaszi <dtavaszi>
2018-02-03 10:30:48 +0000

    fix routing naming

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -9,7 +9,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-import App from '../App'
+import App from '../app.vue'
 import Home from '../components/Home'
 import Hello from '../components/Hello'
 
