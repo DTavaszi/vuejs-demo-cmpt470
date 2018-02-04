ea19497c2f966c984b3d1b3084bde911b95eaf74
dtavaszi <dtavaszi>
2018-02-04 12:01:21 +0000

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/auth/login' to: 'session#login'
+  post '/auth/login', to: 'sessions#login'
 
   root to: 'home#app'
   match "*path", to: "home#app", format: false, via: :get
