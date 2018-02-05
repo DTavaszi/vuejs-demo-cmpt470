2ab8e8d5698a3b64dd7742a81f242011ece9dcd5
dtavaszi <dtavaszi>
2018-02-05 05:47:20 +0000

    migrate changes to DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/auth/login', to: 'sessions#create', defaults: { format: :json }
 
