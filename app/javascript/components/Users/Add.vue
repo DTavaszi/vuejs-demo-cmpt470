aefeb71f41c17de3e7fa12e2e69f0325816db406
dtavaszi <dtavaszi>
2018-02-06 03:35:32 +0000

    Add admin label to checkbox

--- app/javascript/components/Users/Add.vue
+++ app/javascript/components/Users/Add.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="get-user">
-    <input class="form-control"
-            v-model="newUser.name"
-            placeholder="Username">
-    <input class="form-control"
-            v-model="newUser.admin"
-            type="checkbox">
+    <input class="form-control" v-model="newUser.name" placeholder="Username">
+    <label>
+      <input class="form-control" v-model="newUser.admin" type="checkbox">
+      Admin
+    </label>
     <button class="btn btn-primary" @click="addUser"> Add User </button>
   </div>
 </template>
