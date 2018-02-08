5a169c181c0e8b31c00001b77455ade0a40b1f74
dtavaszi <dtavaszi>
2018-02-08 10:45:28 +0000

    Set API_ENDPOINT to heroku url

--- app/javascript/app.config.js
+++ app/javascript/app.config.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // export const AUTH_TOKEN = 'lbUser'
 
-const API_ENDPOINT = "http://localhost:3000"
+//const API_ENDPOINT = "http://localhost:3000"
+const API_ENDPOINT = "https://cmpt470-vuejs.herokuapp.com/"
 
 const HEADERS = function() {
   // const tokenData = JSON.parse(window.localStorage.getItem(AUTH_TOKEN))
