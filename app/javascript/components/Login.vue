cb93ca653b02455c34495c5d195d35c3640290d9
gurpaul <gurpaulgill@gmail.com>
2018-02-11 03:18:24 -0800

    Added temp login page. Made minor changes to registration page

--- app/javascript/components/Login.vue
+++ app/javascript/components/Login.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <h1>Login Page</h1>
     <form v-on:submit.prevent="loginUser()">
@@ -8,10 +8,64 @@
     </form>
     <router-link to="/">Go back</router-link>
   </div>
+</template> -->
+<template>
+  <div>
+    <h1>Temp Login Page</h1>
+      <v-container>
+        <v-layout row>
+          <v-flex xs12 sm6 offset-sm3>
+            <v-card color="grey lighten-4">
+              <v-card-text>
+                <v-container>
+                  <form v-on:submit.prevent="loginUser()">
+                    <v-layout row>
+                      <v-flex xs8>
+                        <v-text-field 
+                          label="Email"
+                          v-model="loginDetails.email"
+                          type="email"
+                          single-line
+                          prepend-icon="email"
+                        ></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                      <v-flex xs8>
+                        <v-text-field
+                          label="Password"
+                          v-model="loginDetails.password"
+                          type="password"
+                          single-line
+                          prepend-icon="lock"
+                        ></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                      <v-flex xs12>
+                        <v-btn color="blue darken-1" type="submit" value="login">Login</v-btn>
+                        <scan>Not registered yet?</scan>
+                        <router-link to="/register">register</router-link>
+                      </v-flex>
+                    </v-layout>
+                  </form>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <router-link to="/">Go back</router-link>
+  </div>
 </template>
+
+
+
+
 
 <script>
 import authentication from './Login/authentication'
+
 
 export default {
   data: function() {
--- app/javascript/components/Register.vue
+++ app/javascript/components/Register.vue
@@ -1,61 +1,62 @@
 <template>
-  <div v-if="loggedIn">
-    <h1>cannot register</h1>
-    <button @click="logout()"> Logout </button>
-  </div>
-  <div v-else>
-    <h1>Registration</h1>
-    <v-container>
-      <v-layout row>
-        <v-flex xs12 sm6 offset-sm3>
-          <v-card>
-            <v-card-text>
-              <v-container>
-                <form v-on:submit.prevent="register()">
-                  <v-layout row>
-                    <v-flex xs12>
-                      <v-text-field
-                        label="Email address"
-                        v-model="newUser.email"
-                        type="email"
-                        required>
-                      </v-text-field>
-                    </v-flex>
-                  </v-layout>
-                  <v-layout row>
-                    <v-flex xs12>
-                      <v-text-field
-                        label="Password"
-                        v-model="newUser.password"
-                        type="password"
-                        required>
-                      </v-text-field>
-                    </v-flex>
-                  </v-layout>
-                  <v-layout row>
-                    <v-flex xs12>
-                      <v-text-field
-                        label="Confirm Password"
-                        v-model="newUser.password_confirmation"
-                        type="password"
-                        >
-                      </v-text-field>
-                    </v-flex>
-                  </v-layout>
-                  <v-layout row>
-                    <v-flex xs12>
-                      <v-btn type="submit" value="register">Register</v-btn>
-                    </v-flex>
-                  </v-layout>
-                </form>
-              </v-container>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <router-link to="/">Go back</router-link>
-  </div>
+    <div v-if="loggedIn">
+      <h1>cannot register</h1>
+      <button @click="logout()"> Logout </button>
+    </div>
+    <div v-else>
+      <h1>Temp Registration Page</h1>
+      <v-container>
+        <v-layout row>
+          <v-flex xs12 sm6 offset-sm3>
+            <v-card color="grey lighten-4">
+              <v-card-text>
+                <v-container>
+                  <form v-on:submit.prevent="register()">
+                    <v-layout row>
+                      <v-flex xs12>
+                        <v-text-field
+                          label="Email"
+                          v-model="newUser.email"
+                          type="email"
+                          prepend-icon="email"
+                          required></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                      <v-flex xs12>
+                        <v-text-field
+                          label="Password"
+                          v-model="newUser.password"
+                          type="password"
+                          prepend-icon="lock"
+                          required></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                      <v-flex xs12>
+                        <v-text-field
+                          label="Confirm Password"
+                          v-model="newUser.password_confirmation"
+                          type="password"
+                          required></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                      <v-flex xs12>
+                        <v-btn color="blue darken-1" type="submit" value="register">Register</v-btn>
+                        <scan>Already have an account?</scan>
+                        <router-link to="/login">Login</router-link>
+                      </v-flex>
+                    </v-layout>
+                  </form>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <router-link to="/">Go back</router-link>
+    </div>
 </template>
 
 
