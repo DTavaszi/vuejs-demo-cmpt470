653d61b24e4dbeb6fdd0523421dfe4d89265ea08
gurpaulg <gurpaulgill@gmail.com>
2018-02-10 21:53:06 -0800

    Logged in users can no longer register. Removed name and id fields in registration

--- app/javascript/components/Register.vue
+++ app/javascript/components/Register.vue
@@ -1,57 +1,61 @@
 <template>
-<div>
-  <h1>Registration</h1>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-card-text>
-            <v-container>
-              <form v-on:submit.prevent="register()">
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-text-field
-                      label="Email address"
-                      v-model="newUser.email"
-                      type="email"
-                      required>
-                    </v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-text-field
-                      label="Password"
-                      v-model="newUser.password"
-                      type="password"
-                      required>
-                    </v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-text-field
-                      label="Confirm Password"
-                      v-model="newUser.password_confirmation"
-                      type="password"
-                      >
-                    </v-text-field>
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                  <v-flex xs12>
-                    <v-btn type="submit" value="register">Register</v-btn>
-                  </v-flex>
-                </v-layout>
-              </form>
-            </v-container>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
-  <router-link to="/">Go back</router-link>
-</div>
+  <div v-if="loggedIn">
+    <h1>cannot register</h1>
+    <button @click="logout()"> Logout </button>
+  </div>
+  <div v-else>
+    <h1>Registration</h1>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <v-card>
+            <v-card-text>
+              <v-container>
+                <form v-on:submit.prevent="register()">
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-text-field
+                        label="Email address"
+                        v-model="newUser.email"
+                        type="email"
+                        required>
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-text-field
+                        label="Password"
+                        v-model="newUser.password"
+                        type="password"
+                        required>
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-text-field
+                        label="Confirm Password"
+                        v-model="newUser.password_confirmation"
+                        type="password"
+                        >
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-btn type="submit" value="register">Register</v-btn>
+                    </v-flex>
+                  </v-layout>
+                </form>
+              </v-container>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <router-link to="/">Go back</router-link>
+  </div>
 </template>
 
 
@@ -66,6 +70,14 @@ export default {
         password: '',
         password_confirmation: '',
       }
+    }
+  },
+  computed: {
+    loggedIn: function() {
+      return this.$store.getters.isLoggedIn
+    },
+    logout: function() {
+      authentication.logout(this)
     }
   },
   methods: {
